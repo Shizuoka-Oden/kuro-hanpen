e1df89671d57c482d3ee99f5787174256fba0deb
mid0111 <m.tajima0111@gmail.com>
2015-12-14 23:03:05 +0900

    Index 削除と Mapping 登録を実装

--- gulp/es.js
+++ gulp/es.js
@@ -6,35 +6,82 @@ var gulp = require('gulp');
 var AWS = require('aws-sdk');
 var through = require('through2');
 
-// var es = require('elasticsearch');
-// var AwsEsConnector = require('http-aws-es');
-// var client = es.Client({
-//   hosts: 'xxx.ap-northeast-1.es.amazonaws.com',
-//   connectionClass: AwsEsConnector,
-//   amazonES: {
-//     region: 'ap-northeast-1',
-//     accessKey: 'AKID',
-//     secretKey: 'secret'
-//   }
-// });
+// elasticsearch client を require すると他の task 定義と競合して(?)
+// TypeError: Cannot redefine property: Symbol(Symbol.iterator)
+// となってしまうため、独自にリクエストを作成
 
 // TODO 設定の外出し
 var esDomain = {
   endpoint: 'xxx.ap-northeast-1.es.amazonaws.com',
   region: 'ap-northeast-1',
-  index: 'test',
-  doctype: 'testType'
+  index: 'test'
 };
 var endpoint =  new AWS.Endpoint(esDomain.endpoint);
 var creds = new AWS.EnvironmentCredentials('AWS');
 
-function uploadDocumentToES() {
+function handleRequest(req, file, callback) {
 
-  // elasticsearch client を require すると他の task 定義と競合して(?)
-  // TypeError: Cannot redefine property: Symbol(Symbol.iterator)
-  // となってしまうため、独自にリクエスト作成
+  // 環境変数の AWS アクセス情報を使用してアクセス
+  var signer = new AWS.Signers.V4(req, 'es');
+  signer.addAuthorization(creds, new Date());
+
+  var send = new AWS.NodeHttpClient();
+  send.handleRequest(req, null, (httpResp) => {
+    var body = '';
+    httpResp.on('data', (chunk) => {
+      body += chunk;
+    });
+    httpResp.on('end', (chunk) => {
+      console.log(body);
+      callback(null, file);
+    });
+  }, (err) => {
+    console.log('Error: ' + err);
+    callback(err, file);
+  });
+}
+
+function deleteIndex() {
   return through.obj((file, enc, callback) => {
+    var req = new AWS.HttpRequest(endpoint);
+    req.method = 'DELETE';
+    req.path = path.join('/', esDomain.index);
+    req.region = esDomain.region;
+    req.headers['Host'] = endpoint.host;
 
+    handleRequest(req, file, callback);
+  });
+}
+
+function putMapping() {
+  return through.obj((file, enc, callback) => {
+    if(file.isNull()) {
+      callback(null, file);
+      return;
+    }
+    if (file.isStream()) {
+      callback(new Error('Streaming not supported'));
+      return;
+    }
+    var filePath = file;
+    if('object' === typeof filePath) {
+      filePath = filePath.path;
+    }
+    var type = path.basename(filePath, '.json');
+
+    var req = new AWS.HttpRequest(endpoint);
+    req.method = 'PUT';
+    req.path = path.join('/', esDomain.index);
+    req.region = esDomain.region;
+    req.headers['Host'] = endpoint.host;
+    req.body = '{mappings: {' + type + ': {properties: {location: {type: "geo_point"}}}}}';
+
+    handleRequest(req, file, callback);
+  });
+}
+
+function uploadDocument() {
+  return through.obj((file, enc, callback) => {
     if(file.isNull()) {
       callback(null, file);
       return;
@@ -44,49 +91,27 @@ function uploadDocumentToES() {
       return;
     }
 
-    // TODO インデックス存在したら消すとかしたい
-    // TODO mapping 定義
     // TODO JSONって配列形式？
-
     var filePath = file;
     if('object' === typeof filePath) {
       filePath = filePath.path;
     }
+    var type = path.basename(filePath, '.json');
     var req = new AWS.HttpRequest(endpoint);
     req.method = 'POST';
-    req.path = path.join('/', esDomain.index, esDomain.doctype);
+    req.path = path.join('/', esDomain.index, type);
     req.region = esDomain.region;
     req.body = fs.readFileSync(filePath);
-    req.headers['presigned-expires'] = false;
     req.headers['Host'] = endpoint.host;
 
-    // TODO AWS のアクセス制御対応
-    // Sign the request (Sigv4)
-    var signer = new AWS.Signers.V4(req, 'es');
-    signer.addAuthorization(creds, new Date());
-
-    var send = new AWS.NodeHttpClient();
-    send.handleRequest(req, null, (httpResp) => {
-      var body = '';
-      httpResp.on('data', (chunk) => {
-        body += chunk;
-      });
-      httpResp.on('end', (chunk) => {
-        console.log(body);
-        callback(null, file);
-      });
-    }, (err) => {
-      console.log('Error: ' + err);
-      callback(err, file);
-    });
-
+    handleRequest(req, file, callback);
   });
 }
 
 gulp.task('regist', function () {
   // TODO data ディレクトリの設定外出し
   gulp.src('data/*.json')
-    .pipe(uploadDocumentToES());
+    .pipe(deleteIndex())
+    .pipe(putMapping())
+    .pipe(uploadDocument());
 });
-
-// gulp.task('build', ['html', 'fonts', 'other']);
