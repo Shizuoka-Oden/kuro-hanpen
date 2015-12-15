a73ddea6a0df0f2a8accd28c05a4a6d573f5e57e
mid0111 <m.tajima0111@gmail.com>
2015-12-15 21:05:22 +0900

    * bulk でリクエストするように修正 * 複数ファイルが存在する場合に１回だけインデックス初期化をするように修正

--- gulp/es.js
+++ gulp/es.js
@@ -6,10 +6,6 @@ var gulp = require('gulp');
 var AWS = require('aws-sdk');
 var through = require('through2');
 
-// elasticsearch client を require すると他の task 定義と競合して(?)
-// TypeError: Cannot redefine property: Symbol(Symbol.iterator)
-// となってしまうため、独自にリクエストを作成
-
 // TODO 設定の外出し
 var esDomain = {
   endpoint: 'xxx.ap-northeast-1.es.amazonaws.com',
@@ -19,38 +15,28 @@ var esDomain = {
 var endpoint =  new AWS.Endpoint(esDomain.endpoint);
 var creds = new AWS.EnvironmentCredentials('AWS');
 
-function handleRequest(req, file, callback) {
+gulp.task('es:init', () => {
+  return deleteIndex()
+    .then(createIndex);
+});
 
-  // 環境変数の AWS アクセス情報を使用してアクセス
-  var signer = new AWS.Signers.V4(req, 'es');
-  signer.addAuthorization(creds, new Date());
-
-  var send = new AWS.NodeHttpClient();
-  send.handleRequest(req, null, (httpResp) => {
-    var body = '';
-    httpResp.on('data', (chunk) => {
-      body += chunk;
-    });
-    httpResp.on('end', (chunk) => {
-      console.log(body);
-      callback(null, file);
-    });
-  }, (err) => {
-    console.log('Error: ' + err);
-    callback(err, file);
-  });
-}
+gulp.task('es:regist', ['es:init'], () => {
+  // TODO data ディレクトリの設定外出し
+  gulp.src('data/*.json')
+    .pipe(putMapping())
+    .pipe(uploadDocument());
+});
 
 function deleteIndex() {
-  return through.obj((file, enc, callback) => {
-    var req = new AWS.HttpRequest(endpoint);
-    req.method = 'DELETE';
-    req.path = path.join('/', esDomain.index);
-    req.region = esDomain.region;
-    req.headers['Host'] = endpoint.host;
+  return new EsRequest().method('DELETE')
+    .path(path.join('/', esDomain.index))
+    .send();
+}
 
-    handleRequest(req, file, callback);
-  });
+function createIndex() {
+  return new EsRequest().method('PUT')
+    .path(path.join('/', esDomain.index))
+    .send();
 }
 
 function putMapping() {
@@ -69,14 +55,16 @@ function putMapping() {
     }
     var type = path.basename(filePath, '.json');
 
-    var req = new AWS.HttpRequest(endpoint);
-    req.method = 'PUT';
-    req.path = path.join('/', esDomain.index);
-    req.region = esDomain.region;
-    req.headers['Host'] = endpoint.host;
-    req.body = '{mappings: {' + type + ': {properties: {location: {type: "geo_point"}}}}}';
-
-    handleRequest(req, file, callback);
+    new EsRequest().method('PUT')
+      .path(path.join('/', esDomain.index, '_mapping', type))
+      .body('{properties: {location: {type: "geo_point"}}}')
+      .send(file)
+      .then((file) => {
+        callback(null, file);
+      })
+      .catch((err) => {
+        callback(err, file);
+      });
   });
 }
 
@@ -91,27 +79,87 @@ function uploadDocument() {
       return;
     }
 
-    // TODO JSONって配列形式？
     var filePath = file;
     if('object' === typeof filePath) {
       filePath = filePath.path;
     }
-    var type = path.basename(filePath, '.json');
-    var req = new AWS.HttpRequest(endpoint);
-    req.method = 'POST';
-    req.path = path.join('/', esDomain.index, type);
-    req.region = esDomain.region;
-    req.body = fs.readFileSync(filePath);
-    req.headers['Host'] = endpoint.host;
-
-    handleRequest(req, file, callback);
+    new EsRequest().method('POST')
+      .path(path.join('/', esDomain.index, '_bulk'))
+      .body(bulk(filePath))
+      .send(file)
+      .then((file) => {
+        callback(null, file);
+      })
+      .catch((err) => {
+        callback(err, file);
+      });
   });
 }
 
-gulp.task('regist', function () {
-  // TODO data ディレクトリの設定外出し
-  gulp.src('data/*.json')
-    .pipe(deleteIndex())
-    .pipe(putMapping())
-    .pipe(uploadDocument());
-});
+function bulk(filePath) {
+  var type = path.basename(filePath, '.json');
+  var data = JSON.parse(fs.readFileSync(filePath));
+  var body = '';
+  var id = 1;
+  if(!Array.isArray(data)) {
+    body += '{ "index" : { "_index" : "' + esDomain.index + '", "_type" : "' + type + '", "_id" : "' + id++ + '" } }\n';
+    body += JSON.stringify(data) + '\n';
+  } else {
+    for(var i = 0 ; i < data.length ; i++ ) {
+      body += '{ "index" : { "_index" : "' + esDomain.index + '", "_type" : "' + type + '", "_id" : "' + id++ + '" } }\n';
+      body += JSON.stringify(data[i]) + '\n';
+    };
+  }
+
+  return body;
+}
+
+// elasticsearch client を require すると他の task 定義と競合して(?)
+// TypeError: Cannot redefine property: Symbol(Symbol.iterator)
+// となってしまうため、独自にリクエストを作成
+class EsRequest {
+  method(method) {
+    this._method = method;
+    return this;
+  }
+  path(urlPath) {
+    this._path = urlPath;
+    return this;
+  }
+  body(body){
+    this._body = body;
+    return this;
+  }
+  send(file) {
+    return new Promise((onFulfilled, onRejected) => {
+      var req = new AWS.HttpRequest(endpoint);
+      req.region = esDomain.region;
+      req.headers['Host'] = endpoint.host;
+      req.method = this._method;
+      req.path = this._path;
+      req.body = this._body;
+
+      // 環境変数の AWS アクセス情報を使用してアクセス
+      var signer = new AWS.Signers.V4(req, 'es');
+      signer.addAuthorization(creds, new Date());
+
+      var send = new AWS.NodeHttpClient();
+      send.handleRequest(req, null, (httpResp) => {
+        var body = '';
+        httpResp.on('data', (chunk) => {
+          body += chunk;
+        });
+        httpResp.on('end', (chunk) => {
+          if(JSON.parse(body).errors) {
+            console.error(body);
+            onRejected(body);
+          }
+          onFulfilled(file);
+        });
+      }, (err) => {
+        console.error(err);
+        onRejected(err);
+      });
+    });
+  }
+}
