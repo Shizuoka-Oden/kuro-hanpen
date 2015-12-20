d5e7594804c85224d27b05800c23e797a57fd95b
Takashi WADA <twada@amy.hi-ho.ne.jp>
2015-12-20 19:59:53 +0900

    Angular Google Maps の表示の e2e-test
    `gulp protractor` が通るようにした。

--- e2e/main.po.js
+++ e2e/main.po.js
@@ -6,10 +6,7 @@
 'use strict';
 
 var MainPage = function() {
-  this.jumbEl = element(by.css('.jumbotron'));
-  this.h1El = this.jumbEl.element(by.css('h1'));
-  this.imgEl = this.jumbEl.element(by.css('img'));
-  this.thumbnailEls = element(by.css('body')).all(by.repeater('awesomeThing in main.awesomeThings'));
+  this.uiGmap = element(by.css('ui-gmap-google-map'));
 };
 
 module.exports = new MainPage();
