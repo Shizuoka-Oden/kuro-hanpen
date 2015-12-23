e895b8a79e2f06f60e08139ccd5c8ca273b336fa
mid0111 <m.tajima0111@gmail.com>
2015-12-23 15:43:22 +0900

    マーカー表示を実装（サーバとの通信はせず固定データを返却）

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,6 +8,7 @@
   /** @ngInject */
   function MainController(GeoLocation) {
     var vm = this;
+    vm.zoom = 14;
     // 航空写真との切り替えやストリートビューの切り替えは無効
     vm.options = {
       mapTypeControl: false,
@@ -16,21 +17,52 @@
     };
 
     vm.map = {};
+
     GeoLocation.getCurrent().then(function(location) {
       vm.map.center = {
         latitude: location.lat,
         longitude: location.lon
       };
-      vm.map.zoom = 14;
     }, function() {
       // 現在地を取得できない場合は静岡駅を中心地に設定
       vm.map = {
         center: {
           latitude: 34.9714699,
           longitude: 138.3869833
-        },
-        zoom: 14
+        }
       };
     });
+
+    GeoLocation.getMarkers().then(function(markers) {
+      vm.markers = markers.map(function(marker) {
+        var icon = getIcon(marker.type);
+        return {
+          id: marker._id,
+          icon: icon,
+          latitude: marker.location.lat,
+          longitude: marker.location.lon
+        };
+      });
+    });
+  }
+
+  function getIcon(type) {
+    switch(type) {
+    case '幼稚園':
+      return 'assets/images/kindergarten.png';
+    case '保育園':
+      return 'assets/images/nursery-school.png';
+    case '小学校':
+      return 'assets/images/primary-school.png';
+    case '中学校':
+      return 'assets/images/junior-high-school.png';
+    case '高校':
+      return 'assets/images/high-school.png';
+    case '公園':
+      return 'assets/images/park.png';
+    case 'ヒヤリハット':
+      return 'assets/images/incident.png';
+    }
+    return '';
   }
 })();
