890fa47cf7b9e16bd2c724dc35a5d0139e37caf2
mid0111 <m.tajima0111@gmail.com>
2015-12-26 23:46:02 +0900

    フィルター用のリスト表示を実装.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,8 +6,10 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController(GeoLocation, $mdSidenav) {
+  function MainController(GeoLocation, FilterLocation, $mdSidenav) {
     var vm = this;
+    vm.filters = FilterLocation.getFilters();
+
     vm.zoom = 14;
     // 航空写真との切り替えやストリートビューの切り替えは無効
     vm.options = {
@@ -33,6 +35,7 @@
       };
     });
 
+    // TODO: フィルタリングのためのtype情報渡す
     GeoLocation.getMarkers().then(function(markers) {
       vm.markers = markers.map(function(marker) {
         var icon = getIcon(marker.type);
@@ -50,6 +53,7 @@
     };
   }
 
+  // TODO: どっか追い出す
   function getIcon(type) {
     switch(type) {
     case '幼稚園':
